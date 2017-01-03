5ea2a06996bee31e974fc52fd5283062a4c87f40
Jake Potter <Jake Potter>
2017-01-03 15:43:20 -0500

    added blink

--- LED.py
+++ LED.py
@@ -1,13 +1,12 @@
 import RPi.GPIO as GPIO
 import time
-
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(14, GPIO.OUT)
-#GPIO.output(14, True)
-#time.sleep(3)
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
-GPIO.output(14, True)
-time.sleep(3)
+blink = 1
+
+while blink == 1:
+    GPIO.output(14, False)
+    time.sleep(1)
+    GPIO.output(14, True)
+    time.sleep(1)
 
