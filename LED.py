b6a5d972d17fbb5e8b5c6554458cecb02893da9a
Jake Potter <Jake Potter>
2017-01-03 15:26:45 -0500

    removed board

--- LED.py
+++ LED.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
+***GPIO.setmode(GPIO.BOARD)
 GPIO.setup(14, GPIO.OUT)
 GPIO.output(14, True)
-time.sleep(3)
+time.sleep(3)***
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
