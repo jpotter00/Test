a05b03cb5badb4a8895556c9a788bcefc4b821df
Jake Potter <Jake Potter>
2017-01-03 16:29:13 -0500

    added counting

--- MotionSensor.py
+++ MotionSensor.py
@@ -4,10 +4,13 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
 
+alert = 1
 pir = MotionSensor(4)
 while True:
     if pir.motion_detected:
         GPIO.output(14, True)
+        print alert
+        alert ++
     else:
         GPIO.output(14, False)
         
