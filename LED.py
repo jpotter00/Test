4919444d462cfb34e7d49cedf7e8f987c8cf8912
Jake Potter <Jake Potter>
2017-01-03 15:23:43 -0500

    mode test

--- LED.py
+++ LED.py
@@ -1,5 +1,13 @@
 import RPi.GPIO as GPIO
+import time
 
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(14, GPIO.OUT)
-
 GPIO.output(14, True)
+time.sleep(3)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+GPIO.output(14, True)
+time.sleep(3)
+
