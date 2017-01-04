4553467e7936aef8f01d35ee217cb14576ba5b43
Jake Potter <Jake Potter>
2017-01-04 15:50:29 -0500

    added blue

--- MotionSensor.py
+++ MotionSensor.py
@@ -3,6 +3,7 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
 GPIO.setup(4,GPIO.IN)
 
 alert = 1
@@ -10,11 +11,17 @@ try:
     while True:
        if GPIO.input(4):
             GPIO.output(14, True)
+            time.sleep(0.25)
+            GPIO.output(14, False)
+            GPIO.output(15, True)
+            time.sleep(0.25)
+            GPIO.output(15, False)
             print "alert number {}".format(alert)
             alert = alert + 1
-            time.sleep(.25)
+            
        else:
-           GPIO.output(14,False)
+           GPIO.output(14, False)
+           GPIO.output(14, False)
         
 except KeyboardInterrupt:
     print " Quit "
